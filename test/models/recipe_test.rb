54f76e371a9254b23f6489a20ddcd93732f6a06c
robotard <robstahendricks@gmail.com>
2016-03-30 18:13:02 +0000

    add tests for recipe (except chef_id)

--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class RecipeTest < ActiveSupport::TestCase
   
-  def Setup
+  def setup
     @recipe = Recipe.new(name: "chicken parm", summary: "Best chicken parm recipe ever", 
               description: "heat oil, add onions, add tomato sauce, add chicken, cook for 20 minutes")
   end
@@ -10,4 +10,52 @@ class RecipeTest < ActiveSupport::TestCase
   test "recipe should be valid" do
     assert @recipe.valid?
   end
+  
+  test "name should be present" do
+    @recipe.name = " "
+    assert_not @recipe.valid?
+  end
+  
+  test "name length should not be too long" do
+    @recipe.name = "a" * 101
+    assert_not @recipe.valid?
+  end
+  
+  test "name length should not be too short" do
+    @recipe.name = "a" * 4
+    assert_not @recipe.valid?
+  end
+  
+  test "summary should be present" do
+    @recipe.summary = " "
+    assert_not @recipe.valid?
+  end
+  
+  test "summary length should not be too long" do
+    @recipe.summary = "a" * 151
+    assert_not @recipe.valid?
+    
+  end
+  
+  test "summary length should not be too short" do
+    @recipe.summary = "a" * 9
+    assert_not @recipe.valid?
+    
+  end
+  
+  test "description should be present" do
+    @recipe.description = " "
+    assert_not @recipe.valid?
+  end
+  
+  test "description length should not be too long" do
+    @recipe.description = "a" * 501
+    assert_not @recipe.valid?
+  end
+  
+  test "description length should not be too short" do
+    @recipe.description = "a" * 19
+    assert_not @recipe.valid?
+  end
+  
 end
